e80cde0d1fcf3eebd2817607c04834644e1c342b
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2026-03-12 02:24:57 +0530

    change the firebase env added

--- serverSide/config/firebase.js
+++ serverSide/config/firebase.js
@@ -1,6 +1,8 @@
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN);
 
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.DATABASE_URL,
